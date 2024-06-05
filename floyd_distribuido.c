80d6de89a43dbc491beb24aae6edd7e5f8fca71e
GPieroni2406 <guzmanpieroni@gmail.com>
2024-06-05 18:08:55 -0300

    Primer version de paralelo y distribuido.

---
+++ floyd_distribuido.c
@@ -0,0 +1,168 @@
+#include "floyd_distribuido.h"
+#include <mpi.h>
+#include <omp.h>
+#include <string.h>
+#include <sys/time.h>
+
+#define INF 9999 // Valor utilizado para representar la ausencia de conexión.
+#define TAMANO_BLOQUE 2 // Tamaño de bloque para la programación dinámica de OpenMP.
+//#define numHilos 4 // Número de hilos para OpenMP.
+
+// Prototipos de funciones
+int **crear_matriz_contigua(int cantidad_vertices);
+int **llenar_matriz(int *cantidad_vertices);
+void mostrar_matriz(int **distancias, int cantidad_vertices);
+void liberar_recursos(int **distancias, int cantidad_vertices);
+void abortar_con_error(int codigo_error);
+
+// Función para crear una matriz de adyacencia contigua en memoria.
+int **crear_matriz_contigua(int cantidad_vertices) {
+    int *datos = (int *)malloc(cantidad_vertices * cantidad_vertices * sizeof(int));
+    int **distancias = (int **)malloc(cantidad_vertices * sizeof(int *));
+
+    for (int i = 0; i < cantidad_vertices; i++) {
+        distancias[i] = &(datos[cantidad_vertices * i]);
+    }
+
+    return distancias;
+}
+
+// Función para llenar la matriz de adyacencia desde un archivo.
+int **llenar_matriz(int *cantidad_vertices) {
+    const char *nombre_archivo = "input.txt";
+    FILE *archivo = fopen(nombre_archivo, "r");
+
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fscanf(archivo, "%d", cantidad_vertices) != 1) {
+        fprintf(stderr, "Error al leer el número de nodos desde el archivo\n");
+        fclose(archivo);
+        exit(EXIT_FAILURE);
+    }
+
+    int **matriz_adyacencia = crear_matriz_contigua(*cantidad_vertices);
+
+    for (int i = 0; i < *cantidad_vertices; i++) {
+        for (int j = 0; j < *cantidad_vertices; j++) {
+            if (fscanf(archivo, "%d", &(matriz_adyacencia[i][j])) != 1) {
+                fprintf(stderr, "Error al leer la matriz de adyacencia desde el archivo\n");
+                free(matriz_adyacencia[0]);
+                free(matriz_adyacencia);
+                fclose(archivo);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    fclose(archivo);
+    return matriz_adyacencia;
+}
+
+// Función para mostrar la matriz de adyacencia.
+void mostrar_matriz(int **distancias, int cantidad_vertices) {
+    printf("Matriz de distancias más cortas entre cada par de vértices:\n");
+    for (int i = 0; i < cantidad_vertices; i++) {
+        for (int j = 0; j < cantidad_vertices; j++) {
+            if (distancias[i][j] == INF)
+                printf("%s\t", "INF");
+            else
+                printf("%d\t", distancias[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Función para liberar los recursos de la matriz de adyacencia.
+void liberar_recursos(int **distancias, int cantidad_vertices) {
+    free(distancias[0]); // Libera el bloque de datos contiguos
+    free(distancias);    // Libera el arreglo de punteros
+}
+
+// Función para abortar la ejecución con un mensaje de error.
+void abortar_con_error(int codigo_error) {
+    if (codigo_error == NEGATIVE_NODE_NUMBER) {
+        fprintf(stderr, "El número de nodos debe ser positivo!\n");
+    } else if (codigo_error == OUT_OF_MEMORY) {
+        fprintf(stderr, "¡Memoria insuficiente!\n");
+    } else if (codigo_error == STEP_LESS_THAN_ONE) {
+        fprintf(stderr, "¡El número de hilos no debe exceder el número de nodos!\n");
+    }
+    abort();
+}
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    int rango, num_procesos, cantidad_vertices;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rango);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procesos);
+
+     // Verifica que se haya proporcionado un argumento para el número de hilos.
+    if (argc != 2) {
+        if (rango == 0) { // Solo el proceso raíz debe imprimir el mensaje de error.
+            fprintf(stderr, "Uso: %s <numero_de_hilos>\n", argv[0]);
+        }
+        MPI_Abort(MPI_COMM_WORLD, 1); // Finaliza todos los procesos.
+    }
+
+    // Convierte el argumento de línea de comandos a un entero para obtener numHilos.
+    int numHilos = atoi(argv[1]);
+    if (numHilos <= 0) {
+        if (rango == 0) { // Solo el proceso raíz debe imprimir el mensaje de error.
+            fprintf(stderr, "El número de hilos debe ser un entero positivo.\n");
+        }
+        MPI_Abort(MPI_COMM_WORLD, 1); // Finaliza todos los procesos.
+    }
+
+    // Verificar que el número de procesos sea adecuado.
+    if (num_procesos == 1) {
+        fprintf(stderr, "¡El número de procesos no puede ser 1!\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    int **distancias;
+    double tiempo_inicio, tiempo_final;
+
+    if (rango == 0) {
+        distancias = llenar_matriz(&cantidad_vertices);
+        mostrar_matriz(distancias, cantidad_vertices);
+        tiempo_inicio = MPI_Wtime();
+    }
+    
+    MPI_Bcast(*distancias, cantidad_vertices * cantidad_vertices, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int paso = cantidad_vertices / num_procesos;
+    int inicio = rango * paso;
+    int fin = inicio + paso;
+    if (rango == num_procesos - 1) {
+        fin = cantidad_vertices;
+    }
+
+    omp_set_num_threads(numHilos);
+    for (int k = 0; k < cantidad_vertices; k++) {
+        #pragma omp parallel for schedule(static, cantVertices/cantHilos) collapse(2)
+        for (int i = inicio; i < fin; i++) {
+            for (int j = 0; j < cantidad_vertices; j++) {
+                if (distancias[i][k] == INF || distancias[k][j] == INF) continue;
+                int nueva_distancia = distancias[i][k] + distancias[k][j];
+                if (nueva_distancia < distancias[i][j]) {
+                    distancias[i][j] = nueva_distancia;
+                }
+            }
+        }
+    }
+    MPI_Allreduce(MPI_IN_PLACE, *distancias, cantidad_vertices * cantidad_vertices, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+    
+
+    if (rango == 0) {
+        tiempo_final = MPI_Wtime();
+        mostrar_matriz(distancias, cantidad_vertices);
+        printf("Tiempo transcurrido: %.6f segundos.\n", tiempo_final - tiempo_inicio);
+    }
+
+    liberar_recursos(distancias, cantidad_vertices);
+    MPI_Finalize();
+    return 0;
+}
---
+++ floyd_paralelo.c
@@ -0,0 +1,152 @@
+#include "floyd_paralelo.h"
+#define CHUNK_SIZE 2
+//#define numHilos 4
+
+// Declaraciones de las funciones utilizadas en el programa.
+void mostrarMatrizDeDistancias(int **matrizDistancias, int cantidadVertices,int numHilos);
+int **leerGrafoDesdeArchivo(const char *nombreArchivo, int *cantidadVertices);
+void algoritmoFloydWarshallParalelo(int **grafo, int cantidadVertices,int numHilos);
+
+// Función para leer la matriz de adyacencia de un grafo desde un archivo.
+int **leerGrafoDesdeArchivo(const char *nombreArchivo, int *cantidadVertices) {
+    // Intenta abrir el archivo de entrada.
+    FILE *archivoEntrada = fopen(nombreArchivo, "r");
+    if (archivoEntrada == NULL) {
+        perror("Error al abrir el archivo");
+        return NULL;
+    }
+
+    // Lee la cantidad de vértices y asigna espacio para el grafo.
+    fscanf(archivoEntrada, "%d", cantidadVertices);
+    int **grafo = (int **)malloc(*cantidadVertices * sizeof(int *));
+    for (int i = 0; i < *cantidadVertices; i++) {
+        grafo[i] = (int *)malloc(*cantidadVertices * sizeof(int));
+        for (int j = 0; j < *cantidadVertices; j++) {
+            // Lee la matriz de adyacencia del grafo desde el archivo.
+            fscanf(archivoEntrada, "%d", &grafo[i][j]);
+        }
+    }
+    fclose(archivoEntrada);
+    return grafo;
+}
+
+// Función que implementa el algoritmo de Floyd-Warshall paralelizado con OpenMP.
+void algoritmoFloydWarshallParalelo(int **grafo, int cantidadVertices,int numHilos) {
+    // Asigna memoria para la matriz de distancias.
+    int **matrizDistancias = (int **)malloc(cantidadVertices * sizeof(int *));
+    for (int i = 0; i < cantidadVertices; i++) {
+        matrizDistancias[i] = (int *)malloc(cantidadVertices * sizeof(int));
+        // Inicializa la matriz de distancias con los valores del grafo.
+        for (int j = 0; j < cantidadVertices; j++) {
+            matrizDistancias[i][j] = grafo[i][j];
+        }
+    }
+
+    // Paraleliza el algoritmo de Floyd-Warshall utilizando OpenMP.
+    for (int k = 0; k < cantidadVertices; k++) {
+    // La directiva collapse(2) combina los dos bucles for anidados (el bucle 'i' y el bucle 'j')
+    // en una sola región de iteraciones. Esto aumenta la granularidad del paralelismo y puede
+    // mejorar la distribución de carga de trabajo, especialmente si el bucle más externo (bucle 'k')
+    // no tiene suficientes iteraciones para mantener ocupados a todos los hilos.
+    // La cláusula schedule(static, cantidadVertices/numHilos) divide las iteraciones de los bucles
+    // combinados en bloques estáticos de tamaño 'cantidadVertices/numHilos' y asigna cada bloque a un hilo diferente.
+    #pragma omp parallel for schedule(static, cantidadVertices/numHilos) collapse(2)
+    for (int i = 0; i < cantidadVertices; i++) {
+        for (int j = 0; j < cantidadVertices; j++) {
+            // Sección crítica para evitar condiciones de carrera cuando varios hilos intentan
+            // actualizar la misma ubicación de memoria de 'matrizDistancias'. La sección crítica
+            // asegura que solo un hilo a la vez pueda ejecutar el bloque de código que actualiza
+            // la matriz, aunque esto puede introducir un cuello de botella y afectar el rendimiento.
+            #pragma omp critical
+            {
+                // Actualiza la matriz de distancias si se encuentra un camino más corto.
+                if (matrizDistancias[i][k] + matrizDistancias[k][j] < matrizDistancias[i][j]) {
+                    matrizDistancias[i][j] = matrizDistancias[i][k] + matrizDistancias[k][j];
+                }
+            }
+        }
+    }
+}
+    #pragma omp single
+    // Imprime la matriz de distancias más cortas.
+    mostrarMatrizDeDistancias(matrizDistancias, cantidadVertices,numHilos);
+
+    // Libera la memoria asignada a la matriz de distancias.
+    for (int i = 0; i < cantidadVertices; i++) {
+        free(matrizDistancias[i]);
+    }
+    free(matrizDistancias);
+}
+
+// Función para imprimir la matriz de distancias más cortas entre cada par de vértices.
+void mostrarMatrizDeDistancias(int **matrizDistancias, int cantidadVertices,int numHilos) {
+    printf("La siguiente matriz muestra las distancias más cortas entre cada par de vértices\n");
+    // Paraleliza la impresión de la matriz de distancias.
+    #pragma omp parallel for schedule(static, cantidadVertices/numHilos)
+    for (int i = 0; i < cantidadVertices; i++) {
+        for (int j = 0; j < cantidadVertices; j++) {
+            // Imprime cada distancia, tratando de manera especial las distancias infinitas.
+            if (matrizDistancias[i][j] == DIS_INFINITA)
+                printf("%s   ", "DIS_INFINITA");
+            else
+                printf("%d     ", matrizDistancias[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// La función principal del programa.
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <numero_de_hilos>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Parsea el número de hilos de los argumentos de la línea de comandos.
+    int numHilos = atoi(argv[1]);
+    if (numHilos <= 0) {
+        fprintf(stderr, "El número de hilos debe ser un entero positivo.\n");
+        exit(EXIT_FAILURE);
+    }
+    int cantidadVertices;
+    // Lee el grafo desde el archivo.
+    int **grafo = leerGrafoDesdeArchivo("input.txt", &cantidadVertices);
+    if (grafo == NULL) {
+        return 1; // Termina si no se pudo leer el grafo.
+    }
+
+    // Variables para medir el tiempo de ejecución.
+    struct timeval tiempoInicio;
+    struct timezone zonaHorariaInicio;
+    struct timeval tiempoFinal;
+    struct timezone zonaHorariaFinal;
+    long tiempoInicioMicroseg, tiempoFinalMicroseg;
+    double tiempoTotal;
+
+    // Establece el número de hilos a utilizar por OpenMP.
+    omp_set_num_threads(numHilos);
+
+    // Comienza la medición del tiempo.
+    gettimeofday(&tiempoInicio, &zonaHorariaInicio);
+
+    // Crea una región paralela y ejecuta el algoritmo de Floyd-Warshall en un solo hilo (esto podría revisarse ya que la región paralela no es necesaria aquí).
+    #pragma omp parallel default(shared)
+        algoritmoFloydWarshallParalelo(grafo, cantidadVertices,numHilos);
+
+    // Termina la medición del tiempo.
+    gettimeofday(&tiempoFinal, &zonaHorariaFinal);
+
+    // Calcula y muestra el tiempo total de ejecución.
+    tiempoInicioMicroseg = tiempoInicio.tv_sec * 1000000 + tiempoInicio.tv_usec;
+    tiempoFinalMicroseg = tiempoFinal.tv_sec * 1000000 + tiempoFinal.tv_usec;
+    tiempoTotal = (tiempoFinalMicroseg - tiempoInicioMicroseg) / 1000000.0;
+    printf("\nTiempo en segundos (t): %lf\n", tiempoTotal);
+
+    // Libera la memoria asignada al grafo.
+    for (int i = 0; i < cantidadVertices; i++) {
+        free(grafo[i]);
+    }
+    free(grafo);
+
+    return 0;
+}
